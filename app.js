2c1b2b8d1807b9b96ae1a9d123037c06e107d36f
Halisson Alves <halisson.eng@gmail.com>
2019-03-04 13:39:34 -0300

    Deleting items from Budget Controller

--- app.js
+++ app.js
@@ -77,6 +77,22 @@ var budgetController = (function() {
             
         },
 
+        deleteItem: function(type, id) {
+
+            var ids, index;
+
+            var ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+
+        },
+
         getBudget: function () {
             return {
                 budget: data.budget,
@@ -233,9 +249,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         if (itemID) {
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
-            
+            budgetCtrl.deleteItem(type, ID);
         }
     };
 
